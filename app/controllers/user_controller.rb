80074ce18c2f551355948862e75b6b576332d79b
smblackwll <spencerblackwell02@gmail.com>
2023-11-08 16:02:46 -0600

    Added some basic stuff into the controller, mostly based on the product controller for now
    Added a list function, a show function, and a create function to the controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,18 @@
 class UserController < ApplicationController
+  def list
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :password_digest))
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
