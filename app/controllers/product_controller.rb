6d3463d3be820b209a2816450d13f82427c858e1
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-09 06:06:38 -0600

    price range selection working

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -16,6 +16,9 @@ class ProductController < ApplicationController
     end
 
     def search
+        # set default value for min and max
+        min_price = params[:min_price].presence || '0'
+        max_price = params[:max_price].presence || '10000000'
         if params[:search].blank?
             @match = Product.all
         else
@@ -23,9 +26,10 @@ class ProductController < ApplicationController
             @match = Product.where("name LIKE ? OR descripton LIKE ?", "%#{@input}%", "%#{@input}%")
             if @match.empty?
                 @match = Product.all
-                flash.now.notice = ("No products found.")       # Makes sure message would disappear after another search
+                flash.now[:notice] = "No products found."     # Makes sure message would disappear after another search
             end
         end
+        @match = @match.where("price >= ? AND price <= ?", min_price, max_price)
         render 'show'
     end 
 end
