53b7246a4b5bed3d92370c819ae6e0b9e2efd941
smblackwll <spencerblackwell02@gmail.com>
2023-11-09 14:34:31 -0600

    Added new userspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with name, email and password' do
+      user = User.new(
+        name: 'testName',
+        email: 'testEmail@email.com',
+        password: "testPass"
+      )
+      expect(user).to be_valid
+    end
+
+  end
 end
