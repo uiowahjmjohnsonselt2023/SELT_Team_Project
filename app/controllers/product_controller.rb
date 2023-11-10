60b82a805d08491ff33e5bf7d1acd04ab60b0720
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-10 11:01:09 -0600

    Added levenshtein distance algorithm, searching working good, price range need improve

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -16,21 +16,54 @@ class ProductController < ApplicationController
     end
 
     def search
-        # set default value for min and max
-        min_price = params[:min_price].presence || '0'
-        max_price = params[:max_price].presence || '10000000'
         if params[:search].blank?
             @match = Product.all
         else
             @input = params[:search]
-            @match = Product.where("name LIKE ? OR descripton LIKE ?", "%#{@input}%", "%#{@input}%")
+            #@match = Product.all.where("name LIKE ? OR descripton LIKE ?", "%#{@input}%", "%#{@input}%").to_a
+            @match = Product.all.select {|product| levenshtein_distance(product.name.downcase, @input.downcase) <= 3 ||
+                                          product.name.downcase.include?(@input) ||
+                                          product.descripton.include?(@input)}
             if @match.empty?
                 @match = Product.all
                 flash.now[:notice] = "No products found."     # Makes sure message would disappear after another search
             end
+            if params[:min_price].present?
+                min_price = params[:min_price]
+                @match = @match.select { |product| product.price >= min_price }
+            end
+            if params[:max_price].present?
+                max_price = params[:max_price]
+                @match = @match.select { |product| product.price <= max_price }
+            end
+            #@match = @match.where("price >= ? AND price <= ?", min_price, max_price)
         end
-        @match = @match.where("price >= ? AND price <= ?", min_price, max_price)
         render 'show'
-    end 
+    end
+
+    # From https://stackoverflow.com/questions/16323571/measure-the-distance-between-two-strings-with-ruby
+    def levenshtein_distance(s, t)
+        m = s.length
+        n = t.length
+        return m if n == 0
+        return n if m == 0
+        d = Array.new(m+1) {Array.new(n+1)}
+
+        (0..m).each {|i| d[i][0] = i}
+        (0..n).each {|j| d[0][j] = j}
+        (1..n).each do |j|
+            (1..m).each do |i|
+                d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
+                              d[i-1][j-1]       # no operation required
+                          else
+                              [ d[i-1][j]+1,    # deletion
+                                d[i][j-1]+1,    # insertion
+                                d[i-1][j-1]+1,  # substitution
+                              ].min
+                          end
+            end
+        end
+        d[m][n]
+    end
 end
 
