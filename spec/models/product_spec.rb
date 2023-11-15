21db689a3356a29cb552f3cedd7f4aa4cf563929
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-14 19:26:24 -0600

    Search tests completed and all passed

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -37,6 +37,43 @@ RSpec.describe Product, type: :model do
     end
   end
 
+  # Testing search feature
+  describe 'search' do
+    before do
+        Product.create(name: 'Apple', price: 0.99)
+        Product.create(name: 'Pineapple', price: 2.44)
+        Product.create(name: 'Guava', price: 3.88)
+    end
+
+    it 'returns products using lowercase for search term' do
+        expect(Product.search('apple')).to include(Product.find_by(name: 'Apple'))
+    end
+
+    it 'returns related products' do
+        expect(Product.search('apple')).to include(Product.find_by(name: 'Apple'))
+        expect(Product.search('apple')).to include(Product.find_by(name: 'Pineapple'))
+        expect(Product.search('apple')).to_not include(Product.find_by(name: 'Guava'))
+    end
+
+    it 'returns the similar product when there is typo' do
+        expect(Product.search('appels')).to include(Product.find_by(name: 'Apple'))
+    end
+
+    it 'returns all products when search term empty' do
+        expect(Product.search('')).to match_array(Product.all)
+    end
+
+
+=begin
+    it 'return matching products that is in the price range' do
+        match = Product.search('', min_price: 1, max_price: 1.5)
+        expect(match).to include(Product.find_by(name: 'Apple'))
+        expect(match).to_not include(Product.find_by(name: 'Pineapple'))
+        expect(match).to_not include(Product.find_by(name: 'Guava'))
+    end
+=end
+
+  end
   # describe 'methods' do
   #   context 'search method' do
   #     it 'returns products with similar names to the search term' do
