1a1254b98ede0bf02e0d2e1e2cd8f0cf012bf73c
James Ostrowski <jamesostrowski21@gmail.com>
2023-11-16 10:09:44 -0600

    signup_controller tests

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -1,7 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "Signups", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET #new" do
+    it "renders the new template" do
+      get signup_path
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns a new user to @user" do
+      get signup_path
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid user params" do
+      it "creates a new user" do
+        valid_params = {
+          user: FactoryBot.attributes_for(:user, name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password")
+        }
+
+        expect {
+          post signup_path, valid_params
+        }.to change(User, :count).by(1)
+      end
+
+      it "signs in the user" do
+        user_attributes = FactoryBot.attributes_for(:user, name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password")
+        post signup_path, user: user_attributes
+        expect(session[:user_id]).to eq(User.last.id)
+      end
+
+      it "redirects to signup_success_path" do
+        valid_params = {
+          user: FactoryBot.attributes_for(:user, name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password")
+        }
+        post signup_path, valid_params
+        expect(response).to redirect_to(signup_success_path)
+      end
+    end
+
+    context "with invalid user params" do
+      it "does not create a new user" do
+        invalid_params = {
+          user: FactoryBot.attributes_for(:user, email: "invalid-email")
+        }
+
+        expect {
+          post signup_path, params: invalid_params
+        }.not_to change(User, :count)
+      end
+
+      it "renders the new template with unprocessable_entity status" do
+        invalid_params = {
+          user: FactoryBot.attributes_for(:user, email: "invalid-email")
+        }
+
+        post signup_path, params: invalid_params
+        expect(response).to render_template(:new)
+        expect(response.status).to eq(422)
+      end
+    end
   end
 end
