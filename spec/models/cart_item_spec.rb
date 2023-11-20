7c0175ff3aea5ec091282218547e6472c630c6f7
KaiLindholm <kailindholm20022@gmail.com>
2023-11-20 11:36:19 -0600

    Added testing to products, and carts

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,42 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  describe 'associations' do
-    it 'belongs to a product' do
-      association = described_class.reflect_on_association(:product)
-      expect(association.macro).to eq :belongs_to
+  describe CartItem do
+    let(:product) { create(:product) }
+    let(:cart) { create(:cart) }
+  
+    it 'validates quantity is present' do
+      cart_item = CartItem.new(quantity: nil, product: product, cart: cart)
+      expect(cart_item.valid?).to eq false
+    end
+  
+    it 'validates quantity is greater than 0' do
+      cart_item = CartItem.new(quantity: 0, product: product, cart: cart)
+      expect(cart_item.valid?).to eq false
+    end
+  
+    it 'validates product_id is present' do
+      cart_item = CartItem.new(product_id: nil, quantity: 1, cart: cart)
+      expect(cart_item.valid?).to eq false
+    end
+  
+    it 'validates cart_id is present' do
+      cart_item = CartItem.new(cart_id: nil, quantity: 1, product: product)
+      expect(cart_item.valid?).to eq false
     end
 
-    it 'belongs to a cart' do
-      association = described_class.reflect_on_association(:cart)
-      expect(association.macro).to eq :belongs_to
+  end
+  
+  describe 'methods ' do
+    let(:product) { create(:product) }
+    let(:cart) { create(:cart) }
+    let(:cart_item) { create(:cart_item, product: product, cart: cart) }
+
+    it 'returns the total price of the cart item' do
+      expect(cart_item.total_price).to eq product.price * cart_item.quantity
+    end
+
+    it 'returns the total quantity of the cart item' do
+      expect(cart_item.total_quantity).to eq cart_item.quantity
     end
   end
-
-  # describe 'validations' do
-  #   it 'validates quantity is present' do
-  #     cartitem = CartItem.new(quantity: nil)
-  #     expect(cartitem.valid?).to eq false
-  #   end
-
-  #   it 'validates quantity is greater than 0' do
-  #     cartitem = CartItem.new(quantity: 0)
-  #     expect(cartitem.valid?).to eq false
-  #   end
-
-  #   it 'validates product_id is present' do
-  #     cartitem = CartItem.new(product_id: nil)
-  #     expect(cartitem.valid?).to eq false
-  #   end
-
-  #   it 'validates cart_id is present' do
-  #     cartitem = CartItem.new(cart_id: nil)
-  #     expect(cartitem.valid?).to eq false
-  #   end
-    
-  #   it 'is valid with valid attributes' do
-  #     cartitem = CartItem.new(quantity: 1, product_id: 1, cart_id: 1)
-  #     expect(cartitem.valid?).to eq true
-  #   end
-  # end
 end
