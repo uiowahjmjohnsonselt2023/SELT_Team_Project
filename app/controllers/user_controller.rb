4988134cdceacb14393138e290dab3e835b95033
smblackwll <spencerblackwell02@gmail.com>
2023-11-20 20:29:31 -0600

    Actually checks if users are logged in before going to profile, otherwise redirect to login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,10 @@
 class UserController < ApplicationController
 
 
+  #ensures users are signed in before we try to do :show, :edit, or :update and redirects them to products
   before_action :ensure_signed_in!, only: [:show, :edit, :update]
+  #
+  before_action :ensure_registration, only: [:index]
   def index
     @user = User.find(params[:id])
   end
