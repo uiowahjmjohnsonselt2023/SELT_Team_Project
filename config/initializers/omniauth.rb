8bf1519e2f4d986df6ded237fe00d320079b6795
James Ostrowski <jamesostrowski21@gmail.com>
2023-11-21 17:27:42 -0600

    omniauth gem

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+ails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+    {
+        scope: 'email',
+        prompt: 'select_account',
+        image_aspect_ratio: 'square',
+        image_size: 50
+    }
+end
+OmniAuth.config.allowed_request_methods = %i[get]
