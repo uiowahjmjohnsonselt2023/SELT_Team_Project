57de57b9ae11b37d66e73d6813687f379aabe6bf
smblackwll <spencerblackwell02@gmail.com>
2023-11-20 21:26:01 -0600

    typo on copied test

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe "Users", type: :request do
       get(users_path)
       expect(response).to redirect_to(signup_path)
     end
-    
+
   end
   describe "Get /show" do
     it "assigns the requested product to @product" do
       user = FactoryBot.create(:user)
-      get show_product_path(user)
+      get show_user_path(user)
       expect(assigns(:user)).to eq(user)
     end
   end
