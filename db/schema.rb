4a030844d5a6c8033a93abdc49f8ba79fe924103
Chase Dittmer <chassedittmer@gmail.com>
2023-11-20 22:02:29 -0600

    added phone_number to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231110045317) do
+ActiveRecord::Schema.define(version: 20231117182434) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20231110045317) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin"
+    t.string   "phone_number"
   end
 
 end
