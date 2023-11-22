61eb64c1d1114e2aefd8a49fac9b70494888a490
Chase Dittmer <chassedittmer@gmail.com>
2023-11-21 21:01:57 -0600

    deleted migrations after rollingback

--- db/migrate/20231122021710_add_user_id_to_addresses.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToAddresses < ActiveRecord::Migration
-  def change
-    add_column :addresses, :userId, :reference
-  end
-end
--- db/migrate/20231122023531_rename_user_id_column_in_addresses.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserIdColumnInAddresses < ActiveRecord::Migration
-  def change
-    rename_column :addresses, :userid, :user_id
-  end
-end
