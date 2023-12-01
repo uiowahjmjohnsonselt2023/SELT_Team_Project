ce9cf3370e1891e05e71ccf2cc30606d58f2f381
Chase Dittmer <chassedittmer@gmail.com>
2023-11-30 23:34:49 -0600

    added a review variable to the table
    Now users can track their reviews and it displays on a users page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231201002020) do
+ActiveRecord::Schema.define(version: 20231201052751) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20231201002020) do
     t.integer  "rating"
     t.boolean  "verified_seller"
     t.string   "phone_number"
+    t.integer  "reviews"
   end
 
 end
