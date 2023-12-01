1fecdf721d28a6a203cd9dcec4e2dec42b62fe06
Xinyi Yuan <xinyi-yuan@uiowa.edu>
2023-11-30 18:27:54 -0600

    factoryBot files

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -6,5 +6,10 @@ FactoryBot.define do
     quantity { 10 }
     user_id {1}
     category_id {3}
+    after(:create) do |product|
+      tag1 = create(:tag, name: "Sample tag1")
+      tag2 = create(:tag, name: "Sample tag2")
+      product.tags << [tag1, tag2]
+    end
   end
 end
