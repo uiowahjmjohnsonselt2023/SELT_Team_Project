dece83b0506f8c61c239d2a076a61c9ec60470bc
James Ostrowski <jamesostrowski21@gmail.com>
2023-12-08 16:29:27 -0600

    remove puts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,12 @@
 module SessionsHelper
     def forget(user)
-        puts "makes it to forget"
         user.forget
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
 
     def remember(user)
-        puts "made it here 2"
         user.remember
-        puts "remember token: #{user.remember_token}"
         cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
     end
