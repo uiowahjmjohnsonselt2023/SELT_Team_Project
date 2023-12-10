449499a9a7956f0678e7d89ed4c09c2156b93761
KaiLindholm <kailindholm20022@gmail.com>
2023-12-10 01:11:31 -0600

    Removed more redundant migrations

--- db/migrate/20231121192303_create_addresses.rb
+++ db/migrate/20231121192303_create_addresses.rb
@@ -1,6 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
+      t.string :address
+      t.string :street
+      t.string :zip
+      t.string :city
+      t.string :country
+      t.string :state
+      
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20231121193423_add_city_to_addresses.rb
+++
@@ -1,5 +0,0 @@
-class AddCityToAddresses < ActiveRecord::Migration
-  def change
-    add_column :addresses, :city, :string
-  end
-end
