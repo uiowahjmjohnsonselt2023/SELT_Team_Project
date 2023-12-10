a06480a8a6e280400e8e300fcc3b0394c726da2d
KaiLindholm <kailindholm20022@gmail.com>
2023-12-10 01:35:38 -0600

    get javascript to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.compress = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
